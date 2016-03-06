cd871ac402d23bd052b24e94a47d7dbb1f1e2be7
Andy <andy.zeng.workhorse@gmail.com>
2016-03-05 20:45:51 -0500

    GPU fusion

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@ export PATH=$PATH:/usr/local/cuda/bin
 CUDA_LIB_DIR=/usr/local/cuda/lib64
 CUDNN_LIB_DIR=/usr/local/cudnn/v4rc/lib64
 
-g++ kinfu.cpp -std=c++11 -Iinclude -Llib -lm -lpng `libpng-config --cflags` -o kinfu
+nvcc kinfu.cu -std=c++11 -Iinclude -Llib -lm -lpng `libpng-config --cflags` -o kinfu
 # g++ multicam.cpp -std=c++11 -Iinclude -Llib -lrealsense -lm -lpng `pkg-config --cflags --libs glfw3 gl glu` `libpng-config --cflags` -o multicam
 # g++ capture.cpp -std=c++11 -Iinclude -Llib -lrealsense -lm -lpng `pkg-config --cflags --libs glfw3 gl glu` `libpng-config --cflags` -o capture
 # g++ train.cpp -std=c++11 -Iinclude -Llib -lm -lpng -L$CUDA_LIB_DIR -L$CUDNN_LIB_DIR `libpng-config --cflags` `pkg-config --cflags opencv` `pkg-config --libs opencv` -o train
