82905c700bb9a995b6860b80576433904880184f
Andy <andy.zeng.workhorse@gmail.com>
2016-03-16 02:21:34 -0400

    include into marvin

--- tools/marvin/compile.sh
+++ tools/marvin/compile.sh
@@ -10,4 +10,4 @@ elif uname | grep -q Linux; then
   CUDNN_LIB_DIR=/usr/local/cudnn/v4rc/lib64
 fi
 
-nvcc -std=c++11 -O3 -o marvin marvin.cu -I/usr/local/cuda/include -I/usr/local/cudnn/v4rc/include -L$CUDA_LIB_DIR -L$CUDNN_LIB_DIR -lcudart -lcublas -lcudnn -lcurand
+nvcc -std=c++11 -O3 -o marvin marvin.cu -I/usr/local/cuda/include -I/usr/local/cudnn/v4rc/include -L$CUDA_LIB_DIR -L$CUDNN_LIB_DIR -lcudart -lcublas -lcudnn -lcurand -lm `libpng-config --cflags` `pkg-config --cflags opencv` `pkg-config --libs opencv`
